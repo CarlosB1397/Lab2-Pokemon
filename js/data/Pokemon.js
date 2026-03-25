cca8a1e314acd6e9b2eec039d372690aa9dd0722
María José Nájera Cervantes <maria.najeracervantes@ucr.ac.cr>
2026-03-25 11:26:01 +0200

    crear clase pokemon

---
+++ js/data/Pokemon.js
@@ -0,0 +1,27 @@
+export class Pokemon{
+    
+    constructor(nombre,id,tipo,altura){
+        this.nombre=nombre;
+        this.id=id;
+        this.tipo=tipo;
+        this.altura=altura;
+    }
+
+    obtenerNombre(){
+        return `Nombre Pokémon: ${this.nombre}`;
+    }
+
+    obtenerId(){
+        return `Id Pokémon: ${this.id}`;
+    }
+
+    obtenerTipo(){
+        return `Tipo Pokémon: ${this.tipo}`;
+    }
+
+    obtenerAltura(){
+        return `Altura Pokémon: ${this.nombre}`;
+    }
+
+}//fin clase
+
