2d1fe1247960b7c19a5133c398671f760e2a8b2c
Jeff <102779432+Jeffrey0522@users.noreply.github.com>
2026-03-27 10:41:26 -0600

    se muestran los primeros 150 pokemones

--- js/data/servicio.js
+++ js/data/servicio.js
@@ -7,13 +7,27 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
             throw new Error('Error al cargar pokemones');
         return res.json();
     })
-    .then(data => {
-        data.results.forEach(pokemon => {
-            pokemones += `<p>Nombre: ${pokemon.name}</p>`;
-        });
-        sugerencias.innerHTML =
-            pokemones;
-        
-        console.log(data.results);
+    .then(async data => {
+        for (const pokemon of data.results) {
+            const pokemonInfo = await obtenerInfo(pokemon.name);
+            pokemones += 
+            `
+                <p>Nombre: ${pokemonInfo.name}</p>
+                <p>height: ${pokemonInfo.height}</p>
+            `;
+        }
+        sugerencias.innerHTML = pokemones;
     })
     .catch(err => console.error(err));
+
+const obtenerInfo = async (pokemon) => {
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`);
+        if(!res.ok)
+            throw new Error('error al encontrar pokemones');
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
