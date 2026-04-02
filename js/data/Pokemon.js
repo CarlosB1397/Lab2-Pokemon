dcd944942e8ede08273259c459a51b7272c4741c
María José Nájera Cervantes <maria.najeracervantes@ucr.ac.cr>
2026-04-02 00:40:41 -0600

    Mejora clase Pokemon agregando limpieza de datos

--- js/data/Pokemon.js
+++ js/data/Pokemon.js
@@ -1,14 +1,28 @@
 export class Pokemon {
 
-    constructor(nombre, id, tipo, altura) {
+    constructor(nombre, id, tipo, altura, imagen) {
         this.nombre = nombre;
         this.id = id;
         this.tipo = tipo;
         this.altura = altura;
+        this.imagen = imagen;
+    }
+
+    static crearPokemon(datos) { //Método para crear un pokemon con sus atributos y realizar limpieza de datos
+        return new Pokemon(
+            datos.name,
+            datos.id,
+            datos.types[0].type.name,
+            datos.height,
+            datos.sprites.other['official-artwork'].front_default);
     }
 
     getNombre() {
         return this.nombre;
+    }
+
+    getImagen() {
+        return this.imagen;
     }
 
     getId() {
