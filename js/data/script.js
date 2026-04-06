36907f98d858bc8c1936605af712b1da9b95a788
CarlosB1397 <carlosbg1397@hotmail.com>
2026-04-06 13:47:07 -0600

    guardar actualizado

--- js/data/script.js
+++ js/data/script.js
@@ -1,26 +1,24 @@
 export class Guardar {
   static KEY = "mis_pokemon";
   static guardarPokemones(pokemones) {
-    localStorage.setItem(this.KEY, JSON.stringify(pokemones));
+    const pokemonesActuales = this.obtenerPokemones();
+    for (const pokemon of pokemones) {
+      if (pokemonesActuales.some((p) => p.name === pokemon.name)) {
+        alert(`El Pokemon ${pokemon.name} ya esta guardado`);
+        return;
+      } //si el pokemon ya esta guardado, mostramos una alerta y salimos de la funcion
+    } //si el pokemon no esta guardado, lo agregamos al array de pokemones actuales
+    const todos = [...pokemonesActuales, ...pokemones];
+    localStorage.setItem(this.KEY, JSON.stringify(todos));
     alert("Pokemon guardado correctamente en el Pokedex");
-    if(localStorage.getItem(this.KEY).length > 5){
-    alert("No puedes guardar más de 6 pokemones en el Pokedex");
-    }//validamos que no se guarden más de 6 pokemones, si se intenta guardar más de 6 pokemones se muestra una alerta
-    for(let i = 0; i < pokemones.length; i++){
-       for(let j = i + 1; j < pokemones.length; j++){
-         if(pokemones[i].name === pokemones[j].name){
-           alert("No puedes guardar el mismo pokemon más de una vez");
-           pokemones.splice(j, 1);
-         }//fin if
-       }//fin for j
-    }//validamos que no se guarden pokemones repetidos, si se encuentra un pokemon repetido se elimina del array
-  }//fin guardarPokemones
+    if (todos.length > 6) {
+      alert("No puedes guardar más de 6 pokemones en el Pokedex");
+    } //si el numero de pokemones guardados es mayor a 6, mostramos una alerta
+  } //fin guardarPokemones
 
   static obtenerPokemones() {
-    const datos= localStorage.getItem(this.KEY);
+    const datos = localStorage.getItem(this.KEY);
     return datos ? JSON.parse(datos) : [];
     //si los datos existen los parseamos, si no existe devolvemos un array vacio
-  };
-
-}//fin clase
-
+  }
+} //fin clase
